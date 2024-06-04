ebb0d3092ccb0c0b2324232ab574e3d26b228cd9
AdRod14 <99355990+AdRod14@users.noreply.github.com>
2024-06-04 14:35:39 -0700

    Update constants.php

--- constants.php
+++ constants.php
@@ -279,9 +279,9 @@ define( 'AI1WM_MESSAGES', 'ai1wm_messages' );
 define( 'AI1WM_SUPPORT_EMAIL', 'support@servmask.com' );
 
 // =================
-// = Max File Size =
+// = Max File Size =  MODIFICADO
 // =================
-define( 'AI1WM_MAX_FILE_SIZE', 536870912 * 20 );
+define( 'AI1WM_MAX_FILE_SIZE', 5368709120 );
 
 // ==================
 // = Max Chunk Size =
